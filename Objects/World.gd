aca4715d9510b65fdec48d1b93b7ded7bd4c9368
Alexzavor <enderdefender9@gmail.com>
2024-11-05 12:56:38 -0700

    A world generator. I think?

--- Objects/World.gd
+++ Objects/World.gd
@@ -17,9 +17,25 @@ func save() -> PackedByteArray:
 Thank you for viewing, for more info, check out the github repo:
 https://github.com/AlexZavor/Raycaster\n".to_utf8_buffer()
 	worldString.append(0)
+	# World Data
+	worldString.append_array("WLD".to_utf8_buffer())
 	worldString.append(worldSize[0])
 	worldString.append(worldSize[1])
 	for y in worldCanvas:
 		for x in y:
 			worldString.append(x)
+	worldString.append(colorPallet.size())
+	for p in colorPallet:
+		for x in p:
+			worldString.append(x)
+	worldString.append(0)
+	# Player Data
+	worldString.append_array("PLR".to_utf8_buffer())
+	worldString.append((player.pos_x[0]/worldSize[0])*255)	# Float elimination, saved as unsigned byte 0-255 = 0-width
+	worldString.append((player.pos_y[0]/worldSize[0])*255)
+	for x in player.color:
+		worldString.append(x)
+	worldString.append(0)
+	#Texture data
+	#Obj Data
 	return worldString
